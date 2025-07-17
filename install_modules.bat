383f2cc6f0f8921552592e5673acf0746348cc4b
DreWulff <awulff98@gmail.com>
2025-07-16 23:46:25 -0400

    Simplified install_modules.bat

--- install_modules.bat
+++ install_modules.bat
@@ -1,10 +1,1 @@
-@echo off
-REM Create virtual environment
-python -m venv venv
-call venv\Scripts\activate
-
-REM Upgrade pip
-python -m pip install --upgrade pip
-
-REM Install required external packages
-pip install python-dotenv public-ip discord.py
+call pip install python-dotenv public-ip discord.py
